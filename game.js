b3763a447cdb6e15a7456e2f0fd11a9488ec3690
Kayla Hunt <huntkay1@msu.edu>
2024-01-19 14:46:28 -0500

    Create getInput function
    Created the getInput function so automate asking the user for their move
    before starting the game.

--- game.js
+++ game.js
@@ -1,10 +1,12 @@
+//Picks a random number between 1 and 3 for the computer's choice
 function getRandomNumber() {
     let min = Math.ceil(1);
     let max = Math.floor(4);
     return Math.floor(Math.random() * (max-min) + 1);
 }
 
-function translateNumber() {
+//Takes the computer's choice and translates it into rock, paper, or scissors
+function computerValue() {
     let computerResponse = getRandomNumber();
 
     if (computerResponse == 1) {
@@ -16,27 +18,37 @@ function translateNumber() {
     }
 }
 
-function theGame(userInput) {
-    computer = translateNumber();
+function getInput() {
+    let userInput = prompt("Rock, Paper, or Scissors?");
+    
+    return userInput.toLowerCase()
+}
 
-    if (computer == userInput) {
-        alert("tie")
-    } else if (computer === "rock" & userInput === "paper") {
-        alert("you win")
-    } else if (computer === "paper" & userInput === "scissors") {
-        alert("you win")
-    } else if (computer === "scissors" & userInput === "rock") {
-        alert("you win")
+//Takes the user's input and the computer's choice and compares them to determine the winner
+function theGame(user, computer) {
+
+    if (computer == user) {
+        alert("tie");
+        let computer = computerValue();
+        let user = getInput();
+        theGame(user, computer)
+    } else if (computer === "rock" & user === "paper") {
+        alert("you win");
+    } else if (computer === "paper" & user === "scissors") {
+        alert("you win");
+    } else if (computer === "scissors" & user === "rock") {
+        alert("you win");
     } else {
-        alert("computer wins")
+        alert("computer wins");
     }
-
 
 }
 
-userInput = prompt("Rock, Paper, or Scissors?")
 
-theGame(userInput.toLowerCase())
+let computer = computerValue();
+let user = getInput();
+
+theGame(user, computer)
 
 
 
